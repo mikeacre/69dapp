2899acd12f944a30eab95a08efcd84980234d993
Mike Acre <mikeacre@gmail.com>
2018-07-29 17:10:21 -0400

    hi

--- client/main.js
+++ client/main.js
@@ -3,10 +3,10 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-var myContract =  new web3.eth.Contract(myAdCardGame.abi, '0x8cfa221be088a9447f6c7c64f0138975e39c0744', {
+/*var myContract =  new web3.eth.Contract(myAdCardGame.abi, '0x8cfa221be088a9447f6c7c64f0138975e39c0744', {
   from: web3.eth.accounts,
   gasPrice: '20000000000'
-});
+});*/
 
 
 
