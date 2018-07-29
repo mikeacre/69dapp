986710f1bcf2796f7d28b4cf07f1a72f39cb3bf4
Mike Acre <mikeacre@gmail.com>
2018-07-29 09:55:26 -0400

    init

--- client/main.js
+++ client/main.js
@@ -6,6 +6,7 @@ import './main.html';
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
+  EthBlocks.init();
 });
 
 Template.hello.helpers({
