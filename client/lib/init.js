536beb1045bd4e60972064555635646bff200abc
Mike Acre <mikeacre@gmail.com>
2018-07-29 09:29:38 -0400

    hii

--- client/lib/init.js
+++ client/lib/init.js
@@ -1,2 +1,3 @@
-if(typeof web3 === 'undefined')
-    web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/3af8bf2f0fc14df1adef0b745e7e09d3'));
+
+//if(typeof web3 === 'undefined')
+  //  web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/3af8bf2f0fc14df1adef0b745e7e09d3'));
